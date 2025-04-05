697d36228afffa35879b2ac0df01c6cd9ee5096e
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-04-05 07:30:52 +0300

    Added gophermart main.go

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"os"
+
+	"github.com/sbilibin2017/go-gophermart/cmd/gophermart/app"
+	"github.com/sbilibin2017/go-gophermart/pkg/cli"
+)
+
+func main() {
+	cmd := app.NewCommand()
+	code := cli.Run(cmd)
+	os.Exit(code)
+}
