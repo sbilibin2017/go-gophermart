cc233a2076fe73290c5f807d43fbeed2a1f5fcb4
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-04-08 05:15:11 +0300

    Added logging to gzip middleware

--- internal/middlewares/gzip.go
+++ internal/middlewares/gzip.go
@@ -5,13 +5,16 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"github.com/sbilibin2017/go-gophermart/internal/log" // импортируем логгер
 )
 
 func GzipMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("Content-Encoding") == "gzip" {
+			log.Info("Request body is gzipped", "method", r.Method, "url", r.URL.Path)
 			gzipReader, err := gzip.NewReader(r.Body)
 			if err != nil {
+				log.Error("Failed to create gzip reader", "method", r.Method, "url", r.URL.Path, "error", err)
 				http.Error(w, "Failed to read gzip data", http.StatusBadRequest)
 				return
 			}
@@ -19,15 +22,18 @@ func GzipMiddleware(next http.Handler) http.Handler {
 			r.Body = gzipReader
 		}
 		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			log.Info("Response will be gzipped", "method", r.Method, "url", r.URL.Path)
 			gzipWriter := gzip.NewWriter(w)
 			w.Header().Set("Content-Encoding", "gzip")
 			defer gzipWriter.Close()
+
 			gzipResponseWriter := &gzipResponseWriter{
 				ResponseWriter: w,
 				Writer:         gzipWriter,
 			}
 			next.ServeHTTP(gzipResponseWriter, r)
 		} else {
+			log.Info("Response will not be gzipped", "method", r.Method, "url", r.URL.Path)
 			next.ServeHTTP(w, r)
 		}
 	})
