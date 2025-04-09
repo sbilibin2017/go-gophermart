70da8d2c1eb09a7a6070ae9ab27bcb3f43670bb1
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-04-09 08:00:08 +0300

    Updated test cov

--- internal/server/server_with_router.go
+++ internal/server/server_with_router.go
@@ -48,9 +48,9 @@ func (s *ServerWithRouter) start(ctx context.Context) error {
 }
 
 func (s *ServerWithRouter) stop(ctx context.Context) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	err := s.server.Shutdown(ctx)
+	err := s.server.Shutdown(shutdownCtx)
 	if err != nil {
 		log.Error("Server stopped with error", "error", err)
 		return err
