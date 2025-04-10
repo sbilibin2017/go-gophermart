116cba0a1d82eacaf5e5ecb21f4a2f146bf6cbd7
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-04-10 07:08:00 +0300

    Added and tested user validators

--- internal/validators/user.go
+++ internal/validators/user.go
@@ -15,24 +15,41 @@ func ValidateLogin(fl validator.FieldLevel) bool {
 
 func ValidatePassword(fl validator.FieldLevel) bool {
 	password := fl.Field().String()
-	flags := []bool{false, false, false, false}
-	if len(password) >= 8 {
-		flags[0] = true
+
+	if len(password) < 8 {
+		return false
 	}
+
+	hasUpper := false
+	hasLower := false
+	hasDigit := false
+	hasSpecial := false
+
 	for _, char := range password {
 		switch {
 		case unicode.IsUpper(char):
-			flags[1] = true
+			hasUpper = true
+		case unicode.IsLower(char):
+			hasLower = true
 		case unicode.IsDigit(char):
-			flags[2] = true
+			hasDigit = true
 		case unicode.IsPunct(char) || unicode.IsSymbol(char):
-			flags[3] = true
+			hasSpecial = true
 		}
 	}
-	for _, ok := range flags {
-		if !ok {
-			return false
-		}
+
+	if !hasUpper {
+		return false
 	}
+	if !hasLower {
+		return false
+	}
+	if !hasDigit {
+		return false
+	}
+	if !hasSpecial {
+		return false
+	}
+
 	return true
 }
