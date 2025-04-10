ccbf6cdc0f26f7af0777c852a114d3b0a18eed4c
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-04-10 19:26:04 +0300

    Added password package for hashing/comparing

---
+++ internal/password/compare.go
@@ -0,0 +1,11 @@
+package password
+
+import "golang.org/x/crypto/bcrypt"
+
+func Compare(hashedPassword, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	if err != nil {
+		return err
+	}
+	return nil
+}
