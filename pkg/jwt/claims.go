14ecfe3b795ff30222163d666005ce1e4cab3562
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-04-10 19:45:22 +0300

    Removed pkg

--- pkg/jwt/claims.go
+++
@@ -1,8 +0,0 @@
-package jwt
-
-import "github.com/golang-jwt/jwt/v4"
-
-type Claims struct {
-	Login string `json:"login"`
-	jwt.RegisteredClaims
-}
--- pkg/unitofwork/errors.go
+++
@@ -1,9 +0,0 @@
-package unitofwork
-
-import "errors"
-
-var (
-	ErrFailedToBeginTransaction    = errors.New("failed to begin transaction")
-	ErrFailedToCommitTransaction   = errors.New("failed to commit transaction")
-	ErrFailedToRollbackTransaction = errors.New("failed to rollback transaction")
-)
