609935b8b4ca7d34cce990284d5dcc94aab188db
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-04-13 16:42:20 +0300

    Added and tested accrual main

--- cmd/accrual/main.go
+++ cmd/accrual/main.go
@@ -1,3 +1,10 @@
 package main
 
-func main() {}
+import (
+	"github.com/sbilibin2017/go-gophermart/cmd/accrual/app"
+	"github.com/sbilibin2017/go-gophermart/internal/cli"
+)
+
+func main() {
+	cli.Run(app.Run)
+}
