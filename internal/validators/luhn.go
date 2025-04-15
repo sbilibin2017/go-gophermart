f36645ec5cf6617a3570ac21dbbcac377967711b
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-04-15 08:56:51 +0300

    Added go validator for luhn validation

--- internal/validators/luhn.go
+++ internal/validators/luhn.go
@@ -3,45 +3,34 @@ package validators
 import (
 	"strconv"
 	"strings"
+
+	"github.com/go-playground/validator/v10"
 )
 
-// LuhnCheck выполняет проверку числа по алгоритму Луна
-func ValidateNumberWithLuhn(number string) bool {
-	// Убираем все пробелы из строки
-	number = strings.ReplaceAll(number, " ", "")
+func ValidateLuhn(fl validator.FieldLevel) bool {
+	number := fl.Field().String()
+	return validateNumberWithLuhn(number)
+}
 
-	// Проверяем, что строка состоит только из цифр
+func validateNumberWithLuhn(number string) bool {
+	number = strings.ReplaceAll(number, " ", "")
 	for _, c := range number {
 		if c < '0' || c > '9' {
 			return false
 		}
 	}
-
-	// Преобразуем строку в массив чисел
 	sum := 0
 	shouldDouble := false
-
-	// Идем с конца по числу
 	for i := len(number) - 1; i >= 0; i-- {
-		// Преобразуем текущий символ в число
 		n, _ := strconv.Atoi(string(number[i]))
-
-		// Удваиваем каждую вторую цифру с конца
 		if shouldDouble {
 			n *= 2
-			// Если результат больше 9, то складываем его цифры
 			if n > 9 {
 				n -= 9
 			}
 		}
-
-		// Добавляем значение к сумме
 		sum += n
-
-		// Переключаем флаг для удвоения цифры
 		shouldDouble = !shouldDouble
 	}
-
-	// Проверяем, делится ли сумма на 10
 	return sum%10 == 0
 }
--- internal/validators/luhn_test.go
+++ internal/validators/luhn_test.go
@@ -3,6 +3,7 @@ package validators
 import (
 	"testing"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/sbilibin2017/go-gophermart/internal/logger"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap/zapcore"
@@ -13,22 +14,29 @@ func init() {
 }
 
 func TestLuhnCheck(t *testing.T) {
+	validate := validator.New()
+	validate.RegisterValidation("luhn", ValidateLuhn)
+
 	tests := []struct {
 		number   string
 		expected bool
 	}{
-		{"4539 1488 0343 6467", true},
-		{"6011514433546201", true},
-		{"123456789", false},
-		{"0000 0000 0000 0000", true},
-		{"1234 5678 9012 3456", false},
-		{"abcd efgh ijkl mnop", false},
+		{"4539 1488 0343 6467", true},  // Корректный номер
+		{"6011514433546201", true},     // Корректный номер
+		{"123456789", false},           // Некорректный номер
+		{"0000 0000 0000 0000", true},  // Корректный номер
+		{"1234 5678 9012 3456", false}, // Некорректный номер
+		{"abcd efgh ijkl mnop", false}, // Некорректный номер (не цифры)
 	}
 
 	for _, test := range tests {
 		t.Run(test.number, func(t *testing.T) {
-			result := ValidateNumberWithLuhn(test.number)
-			assert.Equal(t, test.expected, result)
+			err := validate.Var(test.number, "luhn")
+			if test.expected {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+			}
 		})
 	}
 }
