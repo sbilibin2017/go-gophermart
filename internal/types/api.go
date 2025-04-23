a56177fec7911ba9059c6956cffeea73e8cff957
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-04-23 09:30:20 +0300

    Added types

---
+++ internal/types/api.go
@@ -0,0 +1,6 @@
+package types
+
+type APIStatus struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
---
+++ internal/types/order.go
@@ -0,0 +1,40 @@
+package types
+
+import "time"
+
+type OrderGetRequest struct {
+	Number string `json:"number" validate:"required,luhn"`
+}
+
+type OrderGetResponse struct {
+	Order   string      `json:"order"`
+	Status  OrderStatus `json:"status"`
+	Accrual *int64      `json:"accrual,omitempty"`
+}
+
+type OrderAcceptRequest struct {
+	Order string `json:"order" validate:"required,luhn"`
+	Goods []Good `json:"goods" validate:"required,min=1"`
+}
+
+type Good struct {
+	Description string `json:"description" validate:"required"`
+	Price       int64  `json:"price" validate:"required,gt=0"`
+}
+
+type OrderDB struct {
+	OrderID   string      `db:"order_id"`
+	Status    OrderStatus `db:"status"`
+	Accrual   *int64      `db:"accrual"`
+	CreatedAt time.Time   `db:"created_at"`
+	UpdatedAt time.Time   `db:"updated_at"`
+}
+
+type OrderStatus string
+
+const (
+	OrderStatusRegistered OrderStatus = "REGISTERED"
+	OrderStatusInvalid    OrderStatus = "INVALID"
+	OrderStatusProcessing OrderStatus = "PROCESSING"
+	OrderStatusProcessed  OrderStatus = "PROCESSED"
+)
