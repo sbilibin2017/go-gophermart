0a7269a9e032d0fb7b31f2dd01f4196657ba4aa0
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-04-23 07:01:32 +0300

    Refactoring

--- internal/handlers/helpers/text.go
+++
@@ -1,9 +0,0 @@
-package helpers
-
-import "net/http"
-
-func SendTextResponse(w http.ResponseWriter, message string, status int) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(status)
-	w.Write([]byte(message))
-}
--- internal/types/reward.go
+++
@@ -1,7 +0,0 @@
-package types
-
-type RegisterRewardRequest struct {
-	Match      string `json:"match" validate:"required"`
-	Reward     int64  `json:"reward" validate:"required,gt=0"`
-	RewardType string `json:"reward_type" validate:"required,oneof=% pt"`
-}
