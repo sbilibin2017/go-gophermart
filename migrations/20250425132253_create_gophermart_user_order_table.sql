fcc281d8199d3c706e4285d7f6dd4470cdaa4f6c
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-04-26 19:41:53 +0300

    Added migrations

--- migrations/20250425132253_create_gophermart_user_order_table.sql
+++ migrations/20250425132253_create_gophermart_user_order_table.sql
@@ -1,14 +1,14 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE gophermart_user_order (
-    user_id VARCHAR(100) NOT NULL REFERENCES gophermart_user(login) ON DELETE CASCADE,
-    order_number VARCHAR(100) NOT NULL,
+    login VARCHAR(100) NOT NULL REFERENCES gophermart_user(login) ON DELETE CASCADE,
+    number VARCHAR(100) NOT NULL,
     status VARCHAR(100) NOT NULL,
     accrual BIGINT,
     uploaded_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    PRIMARY KEY (user_id, order_number)
+    PRIMARY KEY (login, number)
 );
 -- +goose StatementEnd
 
